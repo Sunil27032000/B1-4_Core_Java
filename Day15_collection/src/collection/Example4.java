36dd71ed01f28604f121009136e38e8b9ca8920f
sunilkumar <sunilkumar@LAPTOP-JOC7RKCP>
2022-04-17 12:05:20 +0530

    First commit

---
+++ Day15_collection/src/collection/Example4.java
@@ -0,0 +1,17 @@
+package collection;
+
+import java.util.Vector;
+
+public class Example4 {
+
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		@SuppressWarnings("rawtypes")
+		Vector v=new Vector();
+		v.add("Sunil");
+		v.add(15);
+		System.out.println(v);
+	}
+
+}
---
+++ Day15_collection/src/collection/Example5.java
@@ -0,0 +1,18 @@
+ package collection;
+
+import java.util.Stack;
+
+public class Example5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Stack s=new Stack();
+		s.add(45);
+		s.add(15);
+		s.add(26);
+		s.add(96);
+		s.remove(3);
+		System.out.println(s);
+	}
+
+}
